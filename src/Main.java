ce4ff4c7549bf40a8f8215b75af8153d767c2788
Vinícius F. <fonsecavinicius117@gmail.com>
2026-03-18 22:23:37 -0300

    finalização com o Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import service.TaskService;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        TaskService service = new TaskService();
+
+        while (true) {
+            System.out.println("1 - Criar tarefa");
+            System.out.println("2 - Listar tarefas");
+            System.out.println("3 - Mover tarefa");
+            System.out.println("4 - Deletar tarefa");
+            System.out.println("0 - Sair");
+
+            int op = sc.nextInt();
+            sc.nextLine();
+
+            switch (op) {
+                case 1:
+                    System.out.print("Título: ");
+                    String titulo = sc.nextLine();
+
+                    System.out.print("Descrição: ");
+                    String desc = sc.nextLine();
+
+                    service.criarTarefa(titulo, desc);
+                    break;
+
+                case 2:
+                    service.listarTarefas();
+                    break;
+
+                case 3:
+                    System.out.print("ID da tarefa: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Novo status (TODO / DOING / DONE): ");
+                    String status = sc.nextLine();
+
+                    service.moverTarefa(id, status);
+                    break;
+
+                case 4:
+                    System.out.print("ID da tarefa: ");
+                    int deleteId = sc.nextInt();
+
+                    service.deletarTarefa(deleteId);
+                    break;
+
+                case 0:
+                    System.exit(0);
+            }
         }
     }
 }
